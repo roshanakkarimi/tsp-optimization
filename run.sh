b8ce3b510f90d588538cd23af8e787d518604f39
roshanakkarimi <126979902+roshanakkarimi@users.noreply.github.com>
2023-10-18 16:45:20 +0200

    Delete run.sh
    wrong push

--- run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-gcc -ansi -pedantic -Wall -Werror -c /Users/roshanakkarimi/Documents/Projects/OR2/TSP-OR/src/utilities.c -std=c99
-gcc -ansi -pedantic -Wall -Werror -c /Users/roshanakkarimi/Documents/Projects/OR2/TSP-OR/src/main.c -std=c99
-gcc -o main.exe main.o utilities.o
-./main.exe -f ../data/att48.tsp -model_type 1
